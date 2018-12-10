53f4c1da37fb8fc11f8502ac8ee4380a87fdd7fc
Damian Wiśniewski <damian9075@gmail.com>
2018-12-10 18:37:11 +0100

    pseudo rodo message

--- src/pages/admin/layout/main-content/send-email.js
+++ src/pages/admin/layout/main-content/send-email.js
@@ -2,6 +2,23 @@ import React, {Component} from "react";
 import {Form, Row, Input, Textarea, Submit} from '../../../../components/forms/form-components/form-components';
 import PopUp from '../../../../components/pop-up-info/pop-up';
 
+const rodo = `
+\n\n\n\n\n\n\n\n
+___________________________________________
+
+Zgodnie z art. 6 ust. 1 lit. f RODO informujemy, iż Pani/Pana dane osobowe, 
+przekazane nam za pośrednictwem naszej strony internetowej/formularza kontaktowego lub w inny sposób (przekazanie wizytówki, przesłanie poczty elektronicznej, 
+kontakt telefoniczny), są gromadzone, przetwarzane i przechowywane w celu prowadzenia korespondencji z Państwem i w celu, dla którego zostały nam udostępnione.
+Dane osobowe mogą być ujawnione pracownikom lub współpracownikom Spółki, jak też podmiotom udzielającym wsparcia Spółce na zasadzie zleconych usług i zgodnie z zawartymi umowami powierzenia. 
+Informujemy ponadto, że Pani/Pana dane osobowe nie będą przetwarzane w sposób zautomatyzowany i nie będą profilowane.
+
+Posiada Pani/Pan prawo dostępu do treści swoich danych, prawo ich sprostowania, usunięcia, ograniczenia przetwarzania, prawo do przenoszenia danych lub do wniesienia sprzeciwu wobec ich przetwarzania. Osoba, której dane dotyczą, ma prawo wniesienia skargi do PUODO, gdy uzna, iż przetwarzanie jej danych osobowych narusza przepisy RODO.
+
+Podanie danych jest dobrowolne, ale niezbędne do realizacji powyższych celów. Pani/Pana dane będą przetwarzane do czasu ustania celu dla jakiego zostały zgromadzone.
+
+Administrator Danych Osobowych: SmallSoftwareService, ul. Testowa 24b, 11-111 Duże Miasto, tel.: +48 999-888-777, e-mail: wsb.service.api@gmail.com
+`
+
 class SendEmail extends Component {
     constructor(props) {
         super(props);
@@ -9,6 +26,7 @@ class SendEmail extends Component {
         this.state = {
             dataSend: null,
             PopMessage: null,
+            rodo: true,
             emailTo: this.getEmail() || "",
             topic: "",
             content: ""
@@ -28,7 +46,7 @@ class SendEmail extends Component {
 
     clearForm(){
         for(const key in this.state) {
-            if(key === "dataSend" && key === "PopMessage") continue;
+            if(key === "dataSend" && key === "PopMessage" && key === "rodo") continue;
             this.setState({[key]: ""});
         }
     }
@@ -59,7 +77,7 @@ class SendEmail extends Component {
             this.sendData(`http://aplikacja-wsb.herokuapp.com/api/send-email`, {
                 to: this.state.emailTo,
                 subject: this.state.topic,
-                text: this.state.content
+                text: (this.state.rodo ? this.state.content + rodo : this.state.content)
             }).then(() => this.showPopUp())
         } else {
             this.setState({PopMessage: "Uzupełnij wszystkie pola", dataSend: false})
@@ -115,6 +133,10 @@ class SendEmail extends Component {
                         <Textarea label="Treść wiadomości:" col="30" row="12" onChangeField={this.setFormData.bind(this)} target="content" value={this.state.content}/>
                     </Row>
                     <Row className="text-center">
+                        <div className="custom-control custom-checkbox rodo">
+                            <input type="checkbox" className="custom-control-input" id="rodo" onChange={() => { this.setState({rodo: !this.state.rodo})}} checked={this.state.rodo}/>
+                            <label className="custom-control-label" htmlFor="rodo">Dodaj informacje o przetwarzaniu danych osobowych</label>
+                        </div>
                         <Submit value="Wyślij" className="btn btn-primary btn-md" onAccept={this.sendEmail.bind(this)}/>
                     </Row>
                 </Form>
